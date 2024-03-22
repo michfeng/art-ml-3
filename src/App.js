e22f0088c80536a9884c8c8b2bd7bc827ae15308
michfeng <michelle.s.ffeng@gmail.com>
2024-03-22 17:49:46 -0400

    configuring deployment

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <div className="App">
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
         <Router>
-            <NavBar />
+          <NavBar />
             <Routes>
                 <Route exact path="/" element={<Nostalgia />} />
                 <Route path="/nostalgia" element={<Nostalgia />} />
