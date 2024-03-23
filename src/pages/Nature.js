ff123794d169a161c92b6415d2826384d3d31ffb
michfeng <michelle.s.ffeng@gmail.com>
2024-03-23 12:55:58 -0400

    Updates

--- src/pages/Nature.js
+++
@@ -1,8 +0,0 @@
-import { Page } from "../components/Page"
-function Nature () {
-    return (
-        <Page/>
-    )
-}
-
-export default Nature;
--- src/pages/Whimsical.js
+++
@@ -1,7 +0,0 @@
-import { Page } from "../components/Page"
-function Whimsical () {
-    return (
-        <Page/>
-    )
-}
-export default Whimsical;
