b94752b17cf9fa5b26ea013972365c7841ca4082
Rohini Banerjee <rohinibanerjee.rb@gmail.com>
2024-03-24 22:36:50 -0400

    template

--- src/pages/Feral.js
+++ src/pages/Feral.js
@@ -16,6 +16,7 @@ function Feral () {
                 <div style={{ display: 'flex', flexDirection: 'column'}}>
                     <div className="caption" style={{ 'padding-bottom': '35px'}}>The craziest concert ever, held at New York City.</div>
                     <AudioPlayer src={sound1}/>
+                    <text style={{ 'font-family': 'Benne', 'align-items': 'left' }}>Amidst the Friday night frenzy, hungry freshmen immersed in politics, while making out and dancing to EDM. Hangovers loomed as alcohol fueled screams and the rush of a Travis Scott concert. It was a rollercoaster of emotions akin to driving a race car, with drugs and extreme sports tempting the carefree. In the frat basement, chess battles waged alongside sports games, all amidst the haze of alcohol and the thrill of getting drunk or high without a care in the world.</text>
                 </div>
             </div>
             <div className="image-with-caption" style={{'padding-top': '100px', 'padding-left': '150px'}}>
