36ec5ea48ae6f6703f34294ef38aca2e35279358
michfeng <michelle.s.ffeng@gmail.com>
2024-03-24 23:21:49 -0400

    added some of feral

---
+++ src/components/Media.js
@@ -0,0 +1,34 @@
+
+import AudioPlayer from "../components/AudioPlayer";
+
+
+export const Media = ({
+    image, caption, prompt, sound
+}) => {
+    /*return (
+        <div class="row" style={{paddingBottom: '150px'}}>
+            <div class="col-8">
+                <img src={image} alt="Image" width="500px" className="image" />
+            </div>
+            <div class="col-4">
+            <div className="caption" style={{ 'padding-bottom': '35px'}}>{caption}</div>
+                <AudioPlayer src={sound}/>
+                <text style={{ 'font-family': 'Benne', 'align-items': 'left' }}>{prompt}</text>
+            </div>
+        </div>
+    )*/
+
+    return (
+        <div class="row" style={{paddingBottom: '150px'}}>
+            <div class="col-8">
+                <img src={image} alt="Image"  className="image" />
+                <div className="caption" style={{ 'padding-top': '35px'}}>{caption}</div>
+            </div>
+            <div class="col-4">
+                <AudioPlayer src={sound} style={{'padding-bottom': '1000px'}} />
+                <text style={{  'font-family': 'Benne', 'text-align': 'left' }}>{prompt}</text>
+            </div>
+        </div>
+    )
+}
+
--- src/pages/Feral.js
+++ src/pages/Feral.js
@@ -1,33 +1,26 @@
 import img1 from "../images/concert.jpeg";
 import img2 from "../images/wild1.gif";
+import img3 from "../images/wild-cluster2.gif";
+import img4 from "../images/fiery-hot.jpg";
 
 import sound1 from '../audio-files/concert-description.mp3'
 import sound2 from '../audio-files/feral-sculpture-1.mp3'
 
 import '../components/Page.css';
 import AudioPlayer from "../components/AudioPlayer";
+import { Media } from "../components/Media";
 
 
 function Feral () {
     return (
-        <div>
-            <div className="image-with-caption" style={{'padding-top': '100px', 'padding-left': '150px'}}>
-                <img src={img1} alt="Image" width="500" className="image" />
-                <div style={{ display: 'flex', flexDirection: 'column'}}>
-                    <div className="caption" style={{ 'padding-bottom': '35px'}}>The craziest concert ever, held at New York City.</div>
-                    <AudioPlayer src={sound1}/>
-                    <text style={{ 'font-family': 'Benne', 'align-items': 'left' }}>Amidst the Friday night frenzy, hungry freshmen immersed in politics, while making out and dancing to EDM. Hangovers loomed as alcohol fueled screams and the rush of a Travis Scott concert. It was a rollercoaster of emotions akin to driving a race car, with drugs and extreme sports tempting the carefree. In the frat basement, chess battles waged alongside sports games, all amidst the haze of alcohol and the thrill of getting drunk or high without a care in the world.</text>
-                </div>
-            </div>
-            <div className="image-with-caption" style={{'padding-top': '100px', 'padding-left': '150px'}}>
-                <img src={img2} alt="Image" width="525" className="image" />
-                <div style={{ display: 'flex', flexDirection: 'column'}}>
-                    <div className="caption" style={{ 'padding-bottom': '35px'}}>Crowd sculpture.</div>
-                    <AudioPlayer src={sound2}/>
-                </div>
-            </div>
+        
+        <div class="container" style={{paddingTop: '200px'}}>
+            <Media image={img1} caption="The craziest concert ever, held at New York City." sound={sound1} prompt="Amidst the Friday night frenzy, hungry freshmen immersed in politics, while making out and dancing to EDM. Hangovers loomed as alcohol fueled screams and the rush of a Travis Scott concert. It was a rollercoaster of emotions akin to driving a race car, with drugs and extreme sports tempting the carefree. In the frat basement, chess battles waged alongside sports games, all amidst the haze of alcohol and the thrill of getting drunk or high without a care in the world."/>
+            <Media image={img2} caption="Crowd sculpture." sound={sound2} prompt=""/>
+            <Media image={img3} caption="Crowd sculpture." sound={sound2} prompt=""/>
+            <Media image={img4} caption="Crowd sculpture." sound={sound2} prompt=""/>
         </div>
-    )
+    );
 }
 export default Feral;
 
