d23d5d642ba9becc2bf9af51d212a1dab296450a
michfeng <michelle.s.ffeng@gmail.com>
2024-03-24 23:38:38 -0400

    removing pickle

--- src/pages/Nostalgia.js
+++ src/pages/Nostalgia.js
@@ -2,10 +2,7 @@ import { Page } from "../components/Page"
 import Image from "../images/17131.jpg"
 function Nostalgia () {
     return (
-        <div start="200px">
-            <img start="200px" src={Image} width="50px"/>
-            <Page/>
-            
+        <div start="200px">            
         </div>
         
     )
